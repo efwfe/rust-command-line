3fe23315f0dab3552e40ea6bc06fb8df88b89b79
daihui <spartazhangzhang@gamil.com>
2022-12-28 13:13:53 +0800

    headr

---
+++ headr/src/lib.rs
@@ -0,0 +1,123 @@
+use clap::{App, Arg};
+use std::{error::Error};
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+#[derive(Debug)]
+pub struct Config{
+    files: Vec<String>,
+    lines: usize,
+    bytes: Option<usize>,
+}
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+
+pub fn get_args()->MyResult<Config>{
+    let matches = App::new("headr")
+                        .version("0.1.1")
+                        .author("daihui@qq.com")
+                        .about("Rust head")
+                        .arg(
+                            Arg::with_name("lines")
+                            .short("n")
+                            .long("lines")
+                            .value_name("LINES")
+                            .help("Number of lines")
+                            .default_value("10"),
+                        )
+                        .arg(
+                            Arg::with_name("bytes")
+                            .short("c")
+                            .long("bytes")
+                            .value_name("BYTES")
+                            .takes_value(true)
+                            .conflicts_with("lines")
+                            .help("Number of bytes"),
+                        )
+                        .arg(
+                            Arg::with_name("files")
+                            .value_name("FILE")
+                            .help("Input file(s)")
+                            .multiple(true)
+                            .default_value("-"),
+                        )
+                        .get_matches();
+    let lines = matches
+                .value_of("lines")
+                .map(parse_positive_int)
+                .transpose()
+                .map_err(|e| format!("illegal line count -- {}", e))?;
+
+    let bytes = matches
+                .value_of("bytes")
+                .map(parse_positive_int)
+                .transpose() // turn into a Result<Option>
+                .map_err(|e| format!("illegal byte count -- count -- {}", e))?;
+            
+    Ok(Config { 
+        files: matches.values_of_lossy("files").unwrap(),
+        lines: lines.unwrap(),
+        bytes
+    })
+}
+
+pub fn run(config:Config)->MyResult<()>{
+    for filename in config.files{
+        match open(&filename){
+            Err(err)=>eprint!("{}: {}", filename, err),
+            Ok(mut file)=>{
+                if let Some(num_bytes) = config.bytes{
+                    let mut handle = file.take(num_bytes as u64);
+                    let mut buffer = vec![0; num_bytes];
+                    let bytes_read = handle.read(&mut buffer)?;
+                    print!("{}", String::from_utf8_lossy(&buffer[..bytes_read]));
+
+                }else{
+                    let mut line = String::new();
+                    for _ in 0..config.lines{
+                        let bytes = file.read_line(&mut line)?;
+                        if bytes ==0{
+                            break;
+                        }
+                        print!("{}", line);
+                        line.clear();
+                    }
+                }
+                
+            }
+        };
+    }
+    Ok(())
+}
+
+fn open(filename: &str)->MyResult<Box<dyn BufRead>>{
+    match filename {
+        "-" =>Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+
+fn parse_positive_int(val: &str) -> MyResult<usize>{
+    match val.parse(){
+        Ok(n) if n>0 => Ok(n),
+        _ => Err(From::from(val)),
+    }
+}
+
+#[test]
+fn test_parse_positive_int(){
+    let res = parse_positive_int("3");
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap(), 3);
+    // Any string is an error
+    let res = parse_positive_int("foo");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "foo".to_string());
+
+    // A zero is an error
+    let res = parse_positive_int("0");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "0".to_string());
+}
